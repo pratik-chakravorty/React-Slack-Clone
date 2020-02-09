d797c0ced1751e617e81bdd88e04969ca1581e26
Pratik Chakravorty <psman1253@gmail.com>
2020-02-09 00:24:14 +0000

    remove firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,7 @@ import "firebase/database";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDBaTJ6K6A5cikliN_KUQPajbylrapjv0w",
-  authDomain: "react-slack-clone-2707d.firebaseapp.com",
-  databaseURL: "https://react-slack-clone-2707d.firebaseio.com",
-  projectId: "react-slack-clone-2707d",
-  storageBucket: "react-slack-clone-2707d.appspot.com",
-  messagingSenderId: "179541173604",
-  appId: "1:179541173604:web:9de63b37ef7c4ae063c671",
-  measurementId: "G-89XVVWR61W"
+  //add your firebase config here
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
